1eb635df1ed8d7d7feafc6beea14bd545cb8855e
Suyash2008 <72138962+Suyash2008@users.noreply.github.com>
2020-12-08 15:58:02 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,23 @@
+class Pig extends BaseClass{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.Image = loadImage("sprites/enemy.png");
+      this.visibility = 255;
+    }
+    
+    display(){
+      if(this.body.speed < 4){
+        super.display();
+      }
+     else{
+       push();
+       this.visibility = this.visibility -5;
+       World.remove(world,this.body)
+       tint(255,this.visibility)
+       image(this.Image,this.body.position.x,this.body.position.y);
+       pop();
+        }
+      
+    }
+    
+}
